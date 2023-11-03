995f0d0b82c64ea739e118bba259e0cbe09a0f07
pallavi-sahu <pallavisahu0610@gmail.com>
2023-11-03 10:15:14 +0530

    implemented search functionality

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Paper, IconButton} from '@mui/material'
 import { Search } from '@mui/icons-material'
+import { useNavigate } from 'react-router-dom';
 
 const SearchBar = () => {
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+  const handlesubmit = (e) =>{
+    console.log('submit')
+    e.preventDefault();
+  
+    if(searchTerm){
+      
+    }
+  }
+
   return (
     <Paper
     component="form"
-    onSubmit={()=>{}}
+    onSubmit={handlesubmit}
     sx={{
         borderRadius:20,
         border:'1px solid #e3e3e3',
@@ -18,8 +31,8 @@ const SearchBar = () => {
      <input
         className='search-bar'
         placeholder='search...'
-        value=""
-        onChange={()=>{}}
+        value={searchTerm}
+        onChange={(e)=>setSearchTerm(e.target.value)}
      /> 
      <IconButton type='submit' sx={{p:'10px', color:'red'}}>
         <Search/>
--- src/Components/SearchFeed.jsx
+++ src/Components/SearchFeed.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import { Box, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Videos } from "./";
+
+import { fetchFromApi } from "../utils/fetchFromApi";
+import { useParams } from "react-router-dom";
 
 const SearchFeed = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [videos, setvideos] = useState([]);
+  const searchTerm = useParams();
 
-export default SearchFeed
+  useEffect(() => {
+    fetchFromApi(`search?part=snippet&q=${searchTerm}`).then((data) =>
+      setvideos(data.items)
+    );
+  }, [searchTerm]);
+
+  return (
+    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+      <Typography varient="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+       Search Result for <span style={{ color: "#F31503" }}>{searchTerm}</span>videos
+      </Typography>
+      <Videos video={videos} />
+    </Box>
+  );
+};
+
+export default SearchFeed;

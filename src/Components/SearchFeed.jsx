5bfe75ab7d90c24d0093ffc459cb45a79e5b625b
pallavi-sahu <pallavisahu0610@gmail.com>
2023-11-04 15:37:03 +0530

    designed the page for search term

--- src/Components/SearchFeed.jsx
+++ src/Components/SearchFeed.jsx
@@ -6,13 +6,12 @@ import { fetchFromApi } from "../utils/fetchFromApi";
 import { useParams } from "react-router-dom";
 
 const SearchFeed = () => {
-  const [videos, setvideos] = useState([]);
-  const searchTerm = useParams();
+  const [videos, setvideos] = useState(null);
+  const {searchTerm} = useParams();
 
   useEffect(() => {
-    fetchFromApi(`search?part=snippet&q=${searchTerm}`).then((data) =>
-      setvideos(data.items)
-    );
+    fetchFromApi(`search?part=snippet&q=${searchTerm}`)
+      .then((data) => setvideos(data.items))
   }, [searchTerm]);
 
   return (
